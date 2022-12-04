a729397c3eba26f45d74598ee7ac1e9919baf9ec
Codeoutdoors <113397818+Codeoutdoors@users.noreply.github.com>
2022-12-04 12:34:11 -0700

    Creating functions to find user's current location

--- src/script.js
+++ src/script.js
@@ -1,56 +1,4 @@
-function formatDate(date) {
-  let dayList = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[dayList];
-  let minute = ("0" + date.getMinutes()).slice(-2);
-  let hour = ("0" + date.getHours()).slice(-2);
-
-  return `${day} ${hour}:${minute}`;
-}
-
-let timeElement = document.querySelector("#current-time");
-let currentTime = new Date();
-timeElement.innerHTML = formatDate(currentTime);
-
-function displayWeatherInformation(response) {
-  document.querySelector("#searched-temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#searched-type").innerHTML =
-    response.data.weather[0].main;
-  document.querySelector("#searched-wind").innerHTML = Math.round(
-    response.data.wind.speed * 3.6
-  );
-  document.querySelector("#searched-humidity").innerHTML =
-    response.data.main.humidity;
-  document.querySelector("#searched-feels").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
-  document.querySelector("#searched-city").innerHTML = response.data.name;
-}
-
-function searchCity(city) {
-  let apiKey = "4b3503b2f08a729413c4d33ef1186004";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeatherInformation);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-function searchLocation(position) {
+function findMyLocation(position) {
   let apiKey = "4b3503b2f08a729413c4d33ef1186004";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -60,15 +8,8 @@ function searchLocation(position) {
 
 function getCurrentLocation(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+  navigator.geolocation.getCurrentPosition(findMyLocation);
 }
 
-let locationButton = document.querySelector("#location-button");
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-searchCity("Banff");
+let userLocationButton = document.querySelector("#user-location-button");
+userLocationButton.addEventListener("click", getCurrentLocation);
